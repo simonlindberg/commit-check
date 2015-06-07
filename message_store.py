1ca9f21685858d4fe91de249c423f9ed03eaedb2
Simon Lindberg <simon.k.lindberg@gmail.com>
2015-06-07 21:59:04 +0200

    Follow the `pylint` formatting guidelines

--- message_store.py
+++ message_store.py
@@ -8,29 +8,49 @@ Will be used by the 'prepare-commit-msg', 'commit-msg' and 'post-commit' git hoo
 import subprocess
 import os
 
+def run_cmd_output_lines(cmd):
+    """
+    Runs the given command in a new subprocess and returns the output as a list of lines.
+    """
+    process = subprocess.Popen([cmd], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    return process.communicate()[0].split("\n")
+
 def get_msg_file():
-    git_dir = subprocess.Popen(["git rev-parse --git-dir"], stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell = True).communicate()[0].split("\n")[0]
+    """
+    Returns the path to the file where the commit message is stored.
+    The file is located in `$GIT_DIR/last_commit_msg`
+    """
+    git_dir = run_cmd_output_lines("git rev-parse --git-dir")[0]
     msg_file = git_dir + "/last_commit_msg"
 
     return msg_file
 
 def delete():
+    """
+    Deletes the saved commit message.
+    """
     filename = get_msg_file()
     if os.path.exists(filename):
         os.remove(filename)
 
 def save(message):
-    file = open(get_msg_file(), 'w')
-    file.write(message)
-    file.close()
+    """
+    Stores the message to disk.
+    """
+    msg_file = open(get_msg_file(), 'w')
+    msg_file.write(message)
+    msg_file.close()
 
 def get():
+    """
+    Returns the stored commit message.
+    """
     filename = get_msg_file()
 
     if os.path.exists(filename):
-        file = open(filename, 'r')
-        msg = file.read()
-        file.close()
+        msg_file = open(filename, 'r')
+        msg = msg_file.read()
+        msg_file.close()
         return msg
     else:
         return None
